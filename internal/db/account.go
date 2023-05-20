85d837ac00649d7ba66553bab14db5ae70a680a9
vexodeus <vexodeus@gmail.com>
2023-05-20 21:36:39 +0300

    add account deletion methods

--- internal/db/account.go
+++ internal/db/account.go
@@ -15,3 +15,17 @@ func insertAccount(account *Account, db *sql.DB) error {
 	}
 	return nil
 }
+func deleteAccountByEmail(email string, db *sql.DB) error {
+	_, err := db.Exec(deleteAccountByEmailQuery, email)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func deleteAccountByID(id int, db *sql.DB) error {
+	_, err := db.Exec(deleteAccountByIDQuery, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
