e0ce20bd1dee35298df3fbe3451aa7deca1ced83
vexodeus <vexodeus@gmail.com>
2023-05-22 23:17:23 +0300

    fix bugs & modify process function

--- internal/db/task.go
+++ internal/db/task.go
@@ -1,10 +1,12 @@
 package db
 
 type Task struct {
-	ID   int
-	Type int
-	Data int
+	ID     int
+	Type   TaskType
+	Status TaskStatus
+	Data   any
 }
+
 type TaskType int
 
 const (
@@ -13,4 +15,12 @@ const (
 	TASK_TYPE_DELETE
 	TASK_TYPE_GET
 	TASK_TYPE_UPDATE
+)
+
+type TaskStatus int
+
+const (
+	TASK_STATUS_NEW TaskStatus = iota
+	TASK_STATUS_ERROR
+	TASK_STATUS_DONE
 )
--- internal/db/worker.go
+++ internal/db/worker.go
@@ -8,8 +8,8 @@ import (
 type Worker struct {
 	ID     int
 	DB     *sql.DB
-	Input  chan Task
-	Output chan Task
+	Input  chan *Task
+	Output chan *Task
 }
 
 func NewWorker(id int, db *sql.DB) *Worker {
@@ -22,7 +22,7 @@ func (w *Worker) Start(ctx context.Context) {
 	for {
 		select {
 		case task := <-w.Input:
-			result := w.Process(task)
+			result, _ := w.Process(task)
 			w.Output <- result
 		}
 		select {
@@ -31,16 +31,54 @@ func (w *Worker) Start(ctx context.Context) {
 		}
 	}
 }
-func (w *Worker) Process(task Task) Task {
+func (w *Worker) Process(task *Task) (*Task, error) {
+	var err error
 	switch task.Type {
-	case int(TASK_TYPE_INSERT):
+	//
+	case TASK_TYPE_INSERT:
+		switch subject := task.Data.(type) {
 		//
-	case int(TASK_TYPE_DELETE):
+		case Account:
+			err = insertAccount(subject, w.DB)
+			if err != nil {
+				return nil, err
+			}
+		}
 		//
-	case int(TASK_TYPE_GET):
+	//
+	case TASK_TYPE_DELETE:
+		switch subject := task.Data.(type) {
 		//
-	case int(TASK_TYPE_UPDATE):
+		case Account:
+			err = deleteAccountByID(subject.ID, w.DB)
+			if err != nil {
+				return nil, err
+			}
+			// err = deleteAccountByEmail(subject.Email, w.DB)
+			// if err != nil {
+			// 	return nil, err
+			// }
+		}
+		//
+	//
+	case TASK_TYPE_GET:
+		switch subject := task.Data.(type) {
+		//
+		case Account:
+			data, err := getAccountsByID(subject.ID, w.DB)
+			if err != nil {
+				return nil, err
+			}
+			// data, err = getAccountsByEmail(subject.Email, w.DB)
+			// if err != nil {
+			// 	return nil, err
+			// }
+			task.Data = data
+		}
+		//
+	//
+	case TASK_TYPE_UPDATE:
 		//
 	}
-	return task
+	return task, nil
 }
